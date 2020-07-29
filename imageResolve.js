894f4145b37de61fe86b4d064df170a6e2f04864
yoazmenda <yoazmenda@gmail.com>
2020-07-29 17:42:07 +0300

    add image resolving

---
+++ imageResolve.js
@@ -0,0 +1,11 @@
+! // HTTP Request to other domain are blocked
+// We need a CORS proxy
+
+
+// Create an instance of ImageResolver
+// The proxy function is passed as an option
+
+
+function resolverFactory() {
+
+}
